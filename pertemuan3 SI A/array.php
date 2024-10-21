a0268232afcce7e7096c806f54a63fc25e67be5b
adeaprilla5 <162333815+adeaprilla5@users.noreply.github.com>
2024-10-21 15:23:05 +0700

    Add files via upload

---
+++ pertemuan3 SI A/array.php
@@ -0,0 +1,30 @@
+<?php
+$buah = array("apel","pisang","durian","semangka","pepaya","manggis");
+foreach($buah as $buahan){
+    echo $buahan. '<br>';
+
+}
+echo "<hr />";
+$siswa = array("nama" => "ade aprilla","umur" => "19","kota" => "stabat","jurusan" => "Sistem informasi");
+
+echo 'nama siswa :'.$siswa ['nama'].'<br />';
+echo 'umur siswa :'.$siswa ['umur'].'<br />';
+echo 'kota siswa :'.$siswa ['kota'].'<br />';
+echo 'jurusan siswa :'.$siswa ['jurusan'].'<br />';
+?>
+
+<table border="1">
+<tr>
+    <th>nama siswa</th>
+    <th>umur siswa</th>
+    <th>kota siswa</th>
+    <th>jurusan siswa</th>
+</tr>
+<tr>
+    <td><?= $siswa['nama'];?></td>
+    <td><?= $siswa['umur'];?></td>
+    <td><?= $siswa['kota'];?></td>
+    <td><?= $siswa['jurusan'];?></td>
+
+</tr>
+</table>
